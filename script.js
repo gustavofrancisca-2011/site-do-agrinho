a1d6b2afc69d8513294c7249102bb4dd958a0588
gustavofrancisca-2011 <gustavo.francisca@escola.pr.gov.br>
2026-05-12 08:19:52 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const botao = document.getElementById("botaoMensagem");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", () => {
+
+  mensagem.innerHTML =
+    "🌱 Um agro sustentável garante alimento, preservação ambiental e um futuro melhor para todos!";
+
+});
